3dfc64d853be02b2dfdd2b2f1804f0c3370b4bb0
Dominik <46979046+Dominik308@users.noreply.github.com>
2024-05-16 19:16:50 +0200

    Update publish_images.py

--- publish_images.py
+++ publish_images.py
@@ -6,16 +6,24 @@ import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
-
+from sensor_msgs.msg import Image
 class ImagePublisher(Node):
     def __init__(self):
         super().__init__('image_publisher')
-        self.publisher_ = self.create_publisher(Image, 'detectedImage/image', 10)
+        self.publisher_ = self.create_publisher(Image, '/detectedImage/Image', 10)
         self.bridge = CvBridge()
+        self.timer_period = 0.1
+        self.timer = self.create_timer(self.timer_period, self.publish_image)
+        self.cv_image = None
 
-    def publish_image(self, cv_image):
-        msg = self.bridge.cv2_to_imgmsg(cv_image, 'bgr8')
-        self.publisher_.publish(msg)
+    def set_image(self, cv_image):
+        self.cv_image = cv_image
+
+    def publish_image(self):
+        if self.cv_image is not None:
+            msg = self.bridge.cv2_to_imgmsg(self.cv_image, 'bgr8')
+            self.publisher_.publish(msg)
+        print("Published Image---------------------")
 
 def main():
     rclpy.init()
@@ -50,7 +58,19 @@ def main():
                     height = 736
 
                     image_array = np.reshape(image_array, (height, width, 3))
-                    image_publisher.publish_image(image_array)
+                    
+                    # Save the image to a file
+                    #filename = "received_image.jpg"
+                    #cv2.imwrite(filename, image_array)
+                    
+                    image_array = cv2.resize(image_array, (width // 2, height // 2))
+                                       
+                    #image_publisher.publish_image(image_array)
+                    
+                    image_publisher.set_image(image_array)
+                    rclpy.spin_once(image_publisher)
+                    print("Finished Spinning")
+                    
             except Exception as e:
                 print(f"Error receiving or processing image: {e}")
                 if conn:
