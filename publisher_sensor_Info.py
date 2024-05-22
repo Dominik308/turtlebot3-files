89168325e1882af374d5010e7930e1b788e0c53f
Dominik <46979046+Dominik308@users.noreply.github.com>
2024-05-22 08:22:33 +0200

    Update publisher_sensor_Info.py

--- publisher_sensor_Info.py
+++ publisher_sensor_Info.py
@@ -11,7 +11,7 @@ class SensorPublisher(Node):
         super().__init__('SensorPublisher')
         self.sensor = DistanceSensor(9, 10)
         self.bumper = Button(4, pull_up=False)
-        timer_period = 0.2  # seconds
+        timer_period = 0.1  # seconds
 
         self.sonar_publisher = self.create_publisher(String, 'SonarSensorInfo', 10)
         self.sonar_timer = self.create_timer(timer_period, self.publish_sonar)
